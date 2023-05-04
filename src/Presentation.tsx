551efcfcb7ae2ee19bcc8623c16cb7cfea8b6c25
Tania Bezancon <tania.bezancon@epitech.eu>
2023-05-04 15:34:36 +0200

    feat: creation of differents button that can change content in the video

--- src/Presentation.tsx
+++ src/Presentation.tsx
@@ -1,16 +1,23 @@
 import { Fragment } from "react";
 import { AbsoluteFill, Sequence } from "remotion";
-
 interface PresentationProps {
   text: string;
+  color: string;
 }
 
-export const Presentation: React.FC<PresentationProps> = ({ text }) => {
+export const Presentation: React.FC<PresentationProps> = ({ text, color }) => {
+  const styles = {
+    color: color,
+    textAlign: "center",
+    fontSize: "3rem",
+    fontWeight: "bold",
+  };
+
   return (
     <Fragment>
       <AbsoluteFill style={{ background: "white" }}>
         <Sequence from={0} durationInFrames={1800}>
-          <h1>{text}</h1>
+          <h1 style={styles}>{text}</h1>
         </Sequence>
       </AbsoluteFill>
     </Fragment>
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,6 +1,7 @@
 import { CSSProperties, useRef, useState } from "react";
 import { Presentation } from "./Presentation";
 import { Player, PlayerRef } from "@remotion/player";
+import { Img } from "remotion";
 
 const styles: CSSProperties = {
   display: "flex",
@@ -10,17 +11,104 @@ const styles: CSSProperties = {
   width: "20%",
 };
 
+const buttonStyles: CSSProperties = {
+  padding: 10,
+  border: "none",
+  backgroundColor: "#3F51B5",
+  fontWeight: 600,
+  borderRadius: 5,
+  width: "100%",
+  height: 60,
+  fontSize: 18,
+  boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)",
+  cursor: "pointer",
+  margin: 15,
+};
+
+const inputStyles: CSSProperties = {
+  padding: 10,
+  border: "none",
+  color: "#fff",
+  fontWeight: 600,
+  borderRadius: 5,
+  width: "20%",
+  height: 60,
+  fontSize: 18,
+  boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)",
+  cursor: "pointer",
+  marginTop: 10,
+  marginLeft: 300,
+  position: "absolute",
+};
+
+const rectangleStyles: CSSProperties = {
+  backgroundColor: "white",
+  width: "300%",
+  height: "100px",
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "flex-end",
+};
+
+const lineStyles: CSSProperties = {
+  height: "70%",
+  width: "1px",
+  backgroundColor: "black",
+  marginLeft: "33%",
+};
+
 export const RemotionRoot: React.FC = () => {
   const playerRef = useRef<PlayerRef>(null);
   const [text, setText] = useState("");
+  const [color, setColor] = useState("black");
+  const [showImage, setShowImage] = useState(false);
 
   const updateText = (event: React.ChangeEvent<HTMLInputElement>) => {
     setText(event.target.value);
   };
 
+  const changeColor = (newColor: string) => {
+    setColor(newColor);
+  };
+
+  const toggleImage = () => {
+    setShowImage(!showImage);
+  };
+
   return (
     <div style={styles}>
-      <input placeholder="Animation text" onChange={updateText} />
+      <input
+        style={inputStyles}
+        placeholder="Animation text"
+        onChange={updateText}
+      />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button
+          style={{ ...buttonStyles, backgroundColor: "Green" }}
+          onClick={() => changeColor("Green")}
+        />
+        <button
+          style={{ ...buttonStyles, backgroundColor: "Blue" }}
+          onClick={() => changeColor("Blue")}
+        />
+        <button
+          style={{ ...buttonStyles, backgroundColor: "Yellow" }}
+          onClick={() => changeColor("Yellow")}
+        />
+        <button
+          style={{ ...buttonStyles, backgroundColor: "Gray" }}
+          onClick={toggleImage}
+        >
+          {showImage ? "Hide track" : "Show track"}
+        </button>
+      </div>
+      {showImage && (
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={rectangleStyles}>
+            <h1 style={{ color: color}}>{text}</h1>{" "}
+          </div>
+        </div>
+      )}
       <Player
         ref={playerRef}
         component={Presentation}
@@ -29,7 +117,7 @@ export const RemotionRoot: React.FC = () => {
         controls
         durationInFrames={30 * 60}
         fps={60}
-        inputProps={{ text }}
+        inputProps={{ text, color }}
       />
     </div>
   );
